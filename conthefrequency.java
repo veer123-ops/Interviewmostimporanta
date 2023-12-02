904587be821e0abeaa2334d8d4164314d378edcb
veer123-ops <72941982+veer123-ops@users.noreply.github.com>
2023-12-02 12:40:42 +0530

    Update conthefrequency.java

--- conthefrequency.java
+++ conthefrequency.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
-public class conthefrequency {
+public class contnumberfrequency {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
